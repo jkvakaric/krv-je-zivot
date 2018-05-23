dd95f5d41b9858338827e492410f486bd745fa58
davidpodrebarac <podrebarac.david@gmail.com>
2018-05-23 21:47:05 +0200

    dict to list

--- krvjezivot/algorithm.py
+++ krvjezivot/algorithm.py
@@ -1,5 +1,12 @@
 import pandas as pd
+from krvjezivot.constants import *
+
+
+def calculate_who_to_invite(O_MIN, O_MAX, O_Z, P, Z):
+    pass
 
 
 df = pd.read_csv('donors.txt', index_col='id')
 print(df)
+
+
